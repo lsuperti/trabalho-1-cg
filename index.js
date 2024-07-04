bdc26254f63b065ff7d81e4ed6cfe054172cbad0
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2024-07-04 00:11:26 -0300

    Add multi-object import support
    It is now possible to import multiple objects into the scene, there is
    no support yet for scaling, rotation and translation, so it isn't so
    useful yet, but it's evolving quickly.
    
    The process of importing multiple objects consists of calling
    parseAndLoadObj with the path to the obj file, it will return an object
    with fields zNear, zFar, cameraPosition, cameraTarget, objOffset and
    parts, then passing parts along with u_world to the
    renderObjectFromParts function inside the render loop. The other fields
    can also be useful when rendering only one object, their names should be
    pretty self-explanatory, if in doubt check the original code on
    https://webgl2fundamentals.org/webgl/lessons/webgl-load-obj-w-mtl.html.

--- index.js
+++ index.js
@@ -381,161 +381,12 @@ async function main() {
   // compiles and links the shaders, looks up attribute and uniform locations
   const meshProgramInfo = twgl.createProgramInfo(gl, [vs, fs]);
 
-  const objHref = './assets/windmill/windmill.obj';
-  const response = await fetch(objHref);
-  const text = await response.text();
-  const obj = parseOBJ(text);
-  const baseHref = new URL(objHref, window.location.href);
-  const matTexts = await Promise.all(obj.materialLibs.map(async filename => {
-    const matHref = new URL(filename, baseHref).href;
-    const response = await fetch(matHref);
-    return await response.text();
-  }));
-  const materials = parseMTL(matTexts.join('\n'));
-
-  const textures = {
-    defaultWhite: twgl.createTexture(gl, {src: [255, 255, 255, 255]}),
-    defaultNormal: twgl.createTexture(gl, {src: [127, 127, 255, 0]}),
-  };
-
-  // load texture for materials
-  for (const material of Object.values(materials)) {
-    Object.entries(material)
-      .filter(([key]) => key.endsWith('Map'))
-      .forEach(([key, filename]) => {
-        let texture = textures[filename];
-        if (!texture) {
-          const textureHref = new URL(filename, baseHref).href;
-          texture = twgl.createTexture(gl, {src: textureHref, flipY: true});
-          textures[filename] = texture;
-        }
-        material[key] = texture;
-      });
-  }
-
-  // hack the materials so we can see the specular map
-  Object.values(materials).forEach(m => {
-    m.shininess = 25;
-    m.specular = [3, 2, 1];
-  });
-
-  const defaultMaterial = {
-    diffuse: [1, 1, 1],
-    diffuseMap: textures.defaultWhite,
-    normalMap: textures.defaultNormal,
-    ambient: [0, 0, 0],
-    specular: [1, 1, 1],
-    specularMap: textures.defaultWhite,
-    shininess: 400,
-    opacity: 1,
-  };
-
-  const parts = obj.geometries.map(({material, data}) => {
-    // Because data is just named arrays like this
-    //
-    // {
-    //   position: [...],
-    //   texcoord: [...],
-    //   normal: [...],
-    // }
-    //
-    // and because those names match the attributes in our vertex
-    // shader we can pass it directly into `createBufferInfoFromArrays`
-    // from the article "less code more fun".
-
-    if (data.color) {
-      if (data.position.length === data.color.length) {
-        // it's 3. The our helper library assumes 4 so we need
-        // to tell it there are only 3.
-        data.color = { numComponents: 3, data: data.color };
-      }
-    } else {
-      // there are no vertex colors so just use constant white
-      data.color = { value: [1, 1, 1, 1] };
-    }
-
-    // generate tangents if we have the data to do so.
-    if (data.texcoord && data.normal) {
-      data.tangent = generateTangents(data.position, data.texcoord);
-    } else {
-      // There are no tangents
-      data.tangent = { value: [1, 0, 0] };
-    }
-
-    if (!data.texcoord) {
-      data.texcoord = { value: [0, 0] };
-    }
-
-    if (!data.normal) {
-      // we probably want to generate normals if there are none
-      data.normal = { value: [0, 0, 1] };
-    }
-
-    // create a buffer for each array by calling
-    // gl.createBuffer, gl.bindBuffer, gl.bufferData
-    const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
-    const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
-    return {
-      material: {
-        ...defaultMaterial,
-        ...materials[material],
-      },
-      bufferInfo,
-      vao,
-    };
-  });
-
-  function getExtents(positions) {
-    const min = positions.slice(0, 3);
-    const max = positions.slice(0, 3);
-    for (let i = 3; i < positions.length; i += 3) {
-      for (let j = 0; j < 3; ++j) {
-        const v = positions[i + j];
-        min[j] = Math.min(v, min[j]);
-        max[j] = Math.max(v, max[j]);
-      }
-    }
-    return {min, max};
-  }
-
-  function getGeometriesExtents(geometries) {
-    return geometries.reduce(({min, max}, {data}) => {
-      const minMax = getExtents(data.position);
-      return {
-        min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
-        max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
-      };
-    }, {
-      min: Array(3).fill(Number.POSITIVE_INFINITY),
-      max: Array(3).fill(Number.NEGATIVE_INFINITY),
-    });
-  }
-
-  const extents = getGeometriesExtents(obj.geometries);
-  const range = m4.subtractVectors(extents.max, extents.min);
-  // amount to move the object so its center is at the origin
-  const objOffset = m4.scaleVector(
-      m4.addVectors(
-        extents.min,
-        m4.scaleVector(range, 0.5)),
-      -1);
-  const cameraTarget = [0, 0, 0];
-  // figure out how far away to move the camera so we can likely
-  // see the object.
-  const radius = m4.length(range) * 0.5;
-  const cameraPosition = m4.addVectors(cameraTarget, [
-    0,
-    0,
-    radius,
-  ]);
-  // Set zNear and zFar to something hopefully appropriate
-  // for the size of this object.
-  const zNear = radius / 100;
-  const zFar = radius * 3;
-
   function degToRad(deg) {
     return deg * Math.PI / 180;
   }
+
+  const object1 = await parseAndLoadOBJ("./assets/windmill/windmill.obj");
+  const object2 = await parseAndLoadOBJ("./assets/chair/chair.obj");
 
   function render(time) {
     time *= 0.001;  // convert to seconds
@@ -546,11 +397,11 @@ async function main() {
 
     const fieldOfViewRadians = degToRad(60);
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
+    const projection = m4.perspective(fieldOfViewRadians, aspect, object1.zNear, object1.zFar);
 
     const up = [0, 1, 0];
     // Compute the camera's matrix using look at.
-    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
+    const camera = m4.lookAt(object1.cameraPosition, object1.cameraTarget, up);
 
     // Make a view matrix from the camera matrix.
     const view = m4.inverse(camera);
@@ -559,7 +410,7 @@ async function main() {
       u_lightDirection: m4.normalize([-1, 3, 5]),
       u_view: view,
       u_projection: projection,
-      u_viewWorldPosition: cameraPosition,
+      u_viewWorldPosition: object1.cameraPosition,
     };
 
     gl.useProgram(meshProgramInfo.program);
@@ -570,9 +421,18 @@ async function main() {
     // compute the world matrix once since all parts
     // are at the same space.
     let u_world = m4.yRotation(time);
-    u_world = m4.translate(u_world, ...objOffset);
+    u_world = m4.translate(u_world, ...object1.objOffset);
 
-    for (const {bufferInfo, vao, material} of parts) {
+    renderObjectFromParts(u_world, object1.parts);
+    renderObjectFromParts(u_world, object2.parts);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+
+  // TODO: receive scale, rotation, and translation and apply them to the rendered object
+  function renderObjectFromParts(u_world, parts) {
+    for (const { bufferInfo, vao, material } of parts) {
       // set the attributes for this part.
       gl.bindVertexArray(vao);
       // calls gl.uniform
@@ -582,10 +442,160 @@ async function main() {
       // calls gl.drawArrays or gl.drawElements
       twgl.drawBufferInfo(gl, bufferInfo);
     }
-
-    requestAnimationFrame(render);
   }
-  requestAnimationFrame(render);
+
+  async function parseAndLoadOBJ(objHref) {
+    const response = await fetch(objHref);
+    const text = await response.text();
+    const obj = parseOBJ(text);
+    const baseHref = new URL(objHref, window.location.href);
+    const matTexts = await Promise.all(obj.materialLibs.map(async (filename) => {
+      const matHref = new URL(filename, baseHref).href;
+      const response = await fetch(matHref);
+      return await response.text();
+    }));
+    const materials = parseMTL(matTexts.join('\n'));
+
+    const textures = {
+      defaultWhite: twgl.createTexture(gl, { src: [255, 255, 255, 255] }),
+      defaultNormal: twgl.createTexture(gl, { src: [127, 127, 255, 0] }),
+    };
+
+    // load texture for materials
+    for (const material of Object.values(materials)) {
+      Object.entries(material)
+        .filter(([key]) => key.endsWith('Map'))
+        .forEach(([key, filename]) => {
+          let texture = textures[filename];
+          if (!texture) {
+            const textureHref = new URL(filename, baseHref).href;
+            texture = twgl.createTexture(gl, { src: textureHref, flipY: true });
+            textures[filename] = texture;
+          }
+          material[key] = texture;
+        });
+    }
+
+    // hack the materials so we can see the specular map
+    Object.values(materials).forEach(m => {
+      m.shininess = 25;
+      m.specular = [3, 2, 1];
+    });
+
+    const defaultMaterial = {
+      diffuse: [1, 1, 1],
+      diffuseMap: textures.defaultWhite,
+      normalMap: textures.defaultNormal,
+      ambient: [0, 0, 0],
+      specular: [1, 1, 1],
+      specularMap: textures.defaultWhite,
+      shininess: 400,
+      opacity: 1,
+    };
+
+    const parts = obj.geometries.map(({ material, data }) => {
+      // Because data is just named arrays like this
+      //
+      // {
+      //   position: [...],
+      //   texcoord: [...],
+      //   normal: [...],
+      // }
+      //
+      // and because those names match the attributes in our vertex
+      // shader we can pass it directly into `createBufferInfoFromArrays`
+      // from the article "less code more fun".
+      if (data.color) {
+        if (data.position.length === data.color.length) {
+          // it's 3. The our helper library assumes 4 so we need
+          // to tell it there are only 3.
+          data.color = { numComponents: 3, data: data.color };
+        }
+      } else {
+        // there are no vertex colors so just use constant white
+        data.color = { value: [1, 1, 1, 1] };
+      }
+
+      // generate tangents if we have the data to do so.
+      if (data.texcoord && data.normal) {
+        data.tangent = generateTangents(data.position, data.texcoord);
+      } else {
+        // There are no tangents
+        data.tangent = { value: [1, 0, 0] };
+      }
+
+      if (!data.texcoord) {
+        data.texcoord = { value: [0, 0] };
+      }
+
+      if (!data.normal) {
+        // we probably want to generate normals if there are none
+        data.normal = { value: [0, 0, 1] };
+      }
+
+      // create a buffer for each array by calling
+      // gl.createBuffer, gl.bindBuffer, gl.bufferData
+      const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
+      const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+      return {
+        material: {
+          ...defaultMaterial,
+          ...materials[material],
+        },
+        bufferInfo,
+        vao,
+      };
+    });
+
+    function getExtents(positions) {
+      const min = positions.slice(0, 3);
+      const max = positions.slice(0, 3);
+      for (let i = 3; i < positions.length; i += 3) {
+        for (let j = 0; j < 3; ++j) {
+          const v = positions[i + j];
+          min[j] = Math.min(v, min[j]);
+          max[j] = Math.max(v, max[j]);
+        }
+      }
+      return { min, max };
+    }
+
+    function getGeometriesExtents(geometries) {
+      return geometries.reduce(({ min, max }, { data }) => {
+        const minMax = getExtents(data.position);
+        return {
+          min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
+          max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
+        };
+      }, {
+        min: Array(3).fill(Number.POSITIVE_INFINITY),
+        max: Array(3).fill(Number.NEGATIVE_INFINITY),
+      });
+    }
+
+    const extents = getGeometriesExtents(obj.geometries);
+    const range = m4.subtractVectors(extents.max, extents.min);
+    // amount to move the object so its center is at the origin
+    const objOffset = m4.scaleVector(
+      m4.addVectors(
+        extents.min,
+        m4.scaleVector(range, 0.5)),
+      -1);
+    const cameraTarget = [0, 0, 0];
+    // figure out how far away to move the camera so we can likely
+    // see the object.
+    const radius = m4.length(range);
+    const cameraPosition = m4.addVectors(cameraTarget, [
+      0,
+      0,
+      radius,
+    ]);
+    // Set zNear and zFar to something hopefully appropriate
+    // for the size of this object.
+    const zNear = radius / 100;
+    const zFar = radius * 3;
+    return { zNear, zFar, cameraPosition, cameraTarget, objOffset, parts };
+  }
 }
 
 main();
