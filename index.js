ed4997e30a219f1c7741257b804e52864d4641a9
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2024-07-11 21:03:12 -0300

    Remove unused code
    Some code the belonged to the original tutorial code that was used as a
    base was removed. A CSS import was also removed, the CSS controlled some
    parts of the page that aren't in the project since the project only has
    a WebGl canvas. This CSS put a black border on the WebGl canvas, so it
    wasn't completely unused, but that black border isn't desired in this
    project.

--- index.js
+++ index.js
@@ -4,65 +4,6 @@
 import { createNoise2D } from './node_modules/simplex-noise/dist/esm/simplex-noise.js';
 import { renderObject, parseAndLoadOBJ } from './importer.js';
 
-
-function makeIndexIterator(indices) {
-  let ndx = 0;
-  const fn = () => indices[ndx++];
-  fn.reset = () => { ndx = 0; };
-  fn.numElements = indices.length;
-  return fn;
-}
-
-function makeUnindexedIterator(positions) {
-  let ndx = 0;
-  const fn = () => ndx++;
-  fn.reset = () => { ndx = 0; };
-  fn.numElements = positions.length / 3;
-  return fn;
-}
-
-const subtractVector2 = (a, b) => a.map((v, ndx) => v - b[ndx]);
-
-function generateTangents(position, texcoord, indices) {
-  const getNextIndex = indices ? makeIndexIterator(indices) : makeUnindexedIterator(position);
-  const numFaceVerts = getNextIndex.numElements;
-  const numFaces = numFaceVerts / 3;
-
-  const tangents = [];
-  for (let i = 0; i < numFaces; ++i) {
-    const n1 = getNextIndex();
-    const n2 = getNextIndex();
-    const n3 = getNextIndex();
-
-    const p1 = position.slice(n1 * 3, n1 * 3 + 3);
-    const p2 = position.slice(n2 * 3, n2 * 3 + 3);
-    const p3 = position.slice(n3 * 3, n3 * 3 + 3);
-
-    const uv1 = texcoord.slice(n1 * 2, n1 * 2 + 2);
-    const uv2 = texcoord.slice(n2 * 2, n2 * 2 + 2);
-    const uv3 = texcoord.slice(n3 * 2, n3 * 2 + 2);
-
-    const dp12 = twgl.m4.subtract(p2, p1);
-    const dp13 = twgl.m4.subtract(p3, p1);
-
-    const duv12 = subtractVector2(uv2, uv1);
-    const duv13 = subtractVector2(uv3, uv1);
-
-
-    const f = 1.0 / (duv12[0] * duv13[1] - duv13[0] * duv12[1]);
-    const tangent = Number.isFinite(f)
-      ? twgl.v3.normalize(twgl.v3.mulScalar(twgl.v3.subtract(
-          twgl.v3.mulScalar(dp12, duv13[1]),
-          twgl.v3.mulScalar(dp13, duv12[1]),
-        ), f))
-      : [1, 0, 0];
-
-    tangents.push(...tangent, ...tangent, ...tangent);
-  }
-
-  return tangents;
-}
-
 async function main() {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
